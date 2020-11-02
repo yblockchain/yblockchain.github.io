001ffc83989215400ebbd241528ed5c42eadfcc6
Sai Karthik <kasaikarthik@gmail.com>
2020-11-03 00:40:36 +0530

    Changed footer, alert message and corrected Varshini photo source

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
-function validateSubmission(){
-    alert("Thanks for sumitting an entry")
+function validateWaitlistSubmission(){
+	alert("Thank you for signing up. We've added you to the waitlist. You shall receive our resources shortly.")
+}
+
+function validateNewsletterSubmission(){
+	alert("Thank you for subscribing. Feel free to unsubscribe anytime.")
 }
