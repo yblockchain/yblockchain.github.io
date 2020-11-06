204ab7d1910ed975d90ad2f5cac5edbd19e26b28
Sai Karthik <kasaikarthik@gmail.com>
2020-11-06 12:31:19 +0530

    Synced Ambassdor form

--- js/script.js
+++ js/script.js
@@ -5,3 +5,8 @@ function validateWaitlistSubmission(){
 function validateNewsletterSubmission(){
 	alert("Thank you for subscribing. Feel free to unsubscribe anytime.")
 }
+
+function validateAmbassadorSubmission(){
+ 	alert("Thank you for signing up. Our Outreach team shall reach out to you shortly.")
+}
+
